b9aaaba5b5f41954abc5b3e7856f54fc8d0696f7
jrkerr2 <johnrkerr@hotmail.com>
2018-07-13 10:17:39 -0400

    2nd commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+// global variables
+
+ var totalSeconds = 60;
+ var timerVariable; // do I need this?
+ var totalQuestions = 5;
+ var totalScore = 0;     // increment with each correct answer
+ var anwerArray = ["Sally", "George", "Phil", "Bob", "Suzy"];  // fill with 5 correct values
+ var questionArray =[];
+
+
+
+// executes when document is ready
+$(function() {
+
+   
+    // functions
+        // set interval -- eval every 1 second (countdown function)
+
+        // submit after timer expires
+
+        // simple data validation?
+        
+        // compare to actual       
+
+        // write to HTML with results
+	
+})();
