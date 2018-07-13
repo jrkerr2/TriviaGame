8e01041557ca893722b63599264a4e19743faaef
jrkerr2 <johnrkerr@hotmail.com>
2018-07-13 12:25:28 -0400

    low power commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,32 @@
+// need a trivia theme + backgrounds & style
+
 // global variables
 
  var totalSeconds = 60;
  var timerVariable; // do I need this?
  var totalQuestions = 5;
  var totalScore = 0;     // increment with each correct answer
- var anwerArray = ["Sally", "George", "Phil", "Bob", "Suzy"];  // fill with 5 correct values
- var questionArray =[];
+ var anwerArray = ["Roy Moore", "Lionel Messi", "Theodore Roosevelt", "Alexander Hamilton", "Clement Atlee"];  // fill with 5 correct values
+ var questionArray =["Which person below is NOT currently on the Supreme Court?", 
+                    "Which person below is a professional soccer player?", 
+                    "Which person below was a former Vice President of the USA?", 
+                    "Which person below was the first Secretary of the Treasury?", 
+                    "Which person below is a former Prime Minister of the UK?"];
+var incorrectAnswer = ["Bob Smith", 
+                    "Doug Jones", 
+                    "Phoebe Buffay", 
+                    "Bon Scott", 
+                    "Felicity Williams", 
+                    "You", 
+                    "Dread Pirate Roberts"];
 
 
 
 // executes when document is ready
 $(function() {
+
+    // populate questions
+    
 
    
     // functions
