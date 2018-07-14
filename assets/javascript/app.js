72259dc5345f6891cde08bedb2d7d610f3bca555
jrkerr2 <johnrkerr@hotmail.com>
2018-07-14 09:41:42 -0400

    working date timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,41 +3,26 @@
 // global variables
 
  var totalSeconds = 60;
- var timerVariable; // do I need this?
+ var timerVariable = 60; // why do I need this?
+ var timerRunning = false;
  var totalQuestions = 5;
  var totalScore = 0;     // increment with each correct answer
- var anwerArray = ["Roy Moore", "Lionel Messi", "Theodore Roosevelt", "Alexander Hamilton", "Clement Atlee"];  // fill with 5 correct values
- var questionArray =["Which person below is NOT currently on the Supreme Court?", 
-                    "Which person below is a professional soccer player?", 
-                    "Which person below was a former Vice President of the USA?", 
-                    "Which person below was the first Secretary of the Treasury?", 
-                    "Which person below is a former Prime Minister of the UK?"];
-var incorrectAnswer = ["Bob Smith", 
-                    "Doug Jones", 
-                    "Phoebe Buffay", 
-                    "Bon Scott", 
-                    "Felicity Williams", 
-                    "You", 
-                    "Dread Pirate Roberts"];
+
+ var myVar = setInterval(myTimer, 1000);
+
+ function myTimer() {
+    var d = new Date();
+    document.getElementById("timerStatus").innerHTML = d.toLocaleTimeString();
+ }
+
+ myTimer();
+ 
 
 
 
-// executes when document is ready
-$(function() {
 
-    // populate questions
-    
 
-   
-    // functions
-        // set interval -- eval every 1 second (countdown function)
 
-        // submit after timer expires
 
-        // simple data validation?
-        
-        // compare to actual       
-
-        // write to HTML with results
+//$("#randomScore").html("Score needed: " + gameRandom);
 	
-})();
